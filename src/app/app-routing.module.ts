622ff80837ce66c64e689a0c5abb8d84f29d4248
Shawnarun <shawnarun19@gmail.com>
2023-02-22 04:42:40 +0530

    Pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { CareerComponent } from './pages/career/career.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:WelcomeComponent},
+  {path:'service',component:ServiceComponent},
+  {path:'career',component:CareerComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CareerComponent } from './pages/career/career.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    WelcomeComponent,
+    ServiceComponent,
+    ContactComponent,
+    CareerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
